f200d52c211da604417e7579754ae8e670a1774e
accdaune <accdaune@gmail.com>
2025-10-27 21:08:42 +0700

    Add files via upload

---
+++ volcano.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Auto Bypass Volcano
+// @namespace    http://tampermonkey.net/
+// @version      v1.0.0.0
+// @description  You only need to solve the captcha
+// @author       TC bình 
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @icon         https://i.postimg.cc/y6Gx5NBD/volcano.png
+// @downloadURL  https://github.com/accdaune/Bypass/blob/main/volcano.user.js
+// @updateURL    https://github.com/accdaune/Bypass/blob/main/volcano.user.js
+// @require      https://github.com/accdaune/Bypass/blob/main/bypassvolcano.js
+// ==/UserScript==
