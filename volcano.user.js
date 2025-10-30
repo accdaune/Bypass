61a3334d19c8ea54245af844c54f801c214a25ac
accdaune <accdaune@gmail.com>
2025-10-31 02:21:46 +0700

    Add files via upload

---
+++ volcano.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Volcano & Work.ink Bypass
+// @namespace    http://tampermonkey.net/
+// @version      1.0.1
+// @description  Bypass Volcano and Work.ink 
+// @author       TC bình 
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @icon         https://i.postimg.cc/g2NMbY7k/file-0000000074086209922d93c197850004.png
+// @downloadURL  https://github.com/accdaune/Bypass/raw/main/volcano.user.js
+// @updateURL    https://github.com/accdaune/Bypass/raw/main/volcano.user.js
+// @require      https://github.com/accdaune/Bypass/raw/main/bypassvolcano.js
+// ==/UserScript==
