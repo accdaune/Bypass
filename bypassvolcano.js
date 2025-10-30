e59ecc6355ddc575ff93d8bef123e1d4fb796d0f
accdaune <accdaune@gmail.com>
2025-10-31 03:43:42 +0700

    Update bypassvolcano.js

--- bypassvolcano.js
+++ bypassvolcano.js
@@ -121,7 +121,7 @@
                     content: '☠️';
                     position: absolute;
                     top: 10px;
-                    right: 80px;
+                    right: 20px;
                     font-size: 24px;
                     opacity: 0.7;
                     z-index: 1;
@@ -978,5 +978,6 @@
         ob.observe(document.documentElement, { childList: true, subtree: true });
     }
 })();
+
 
 
